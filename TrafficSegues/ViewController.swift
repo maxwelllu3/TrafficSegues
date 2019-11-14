ea680e5068dcedfb85f64bf4a7d523ed8f80966a
Maxwell Lu <blu@lcs.on.ca>
2019-11-13 19:18:03 -0500

    Initial Commit

---
+++ TrafficSegues/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrafficSegues
+//
+//  Created by Maxwell Lu on 2019-11-13.
+//  Copyright © 2019 Maxwell Lu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
